0b4b64207443299d1a89de95c0cefabdde9cafa2
Harry <naingaungluu@coidgo.co>
2024-06-07 17:34:36 +0700

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,97 @@
+import dev.shreyaspatil.bytemask.core.encryption.EncryptionSpec
+import dev.shreyaspatil.bytemask.plugin.config.KeySource
+
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.byteMask)
+}
+
+android {
+    namespace = "co.codigo.demo.maskedkeys"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "co.codigo.demo.maskedkeys"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("../keystore/release.keystore")
+            storePassword = "release"
+            keyAlias = "releasekeystore"
+            keyPassword = "release"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+bytemaskConfig {
+    // Strings to read from
+    defaultPropertiesFileName.set("secure.properties")
+
+    // Class name for the generated class
+    className.set("MaskedConfig")
+
+    configure("release") {
+        enableEncryption = true
+        encryptionSpec.set(EncryptionSpec(algorithm = "AES", transformation = "AES/GCM/NoPadding"))
+        encryptionKeySource = KeySource.Key("DecryptionKey")
+    }
+}
+
+dependencies {
+    implementation(kotlin("reflect"))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ app/src/main/java/co/codigo/demo/maskedkeys/MainActivity.kt
@@ -0,0 +1,86 @@
+package co.codigo.demo.maskedkeys
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.text.buildSpannedString
+import co.codigo.demo.maskedkeys.ui.theme.MaskedKeysDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val secureKeysMap = MaskedConfig::class.members
+            .filter { it.isFinal }
+            .associate {
+                val value = it.call(MaskedConfig).toString()
+                it.name to value
+            }
+
+        setContent {
+            MaskedKeysDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        KeyValueLayout(secureKeysMap)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun KeyValueLayout(list: Map<String,String>) {
+    LazyColumn {
+        items(list.toList(), key = { it.first }) { pair ->
+            KeyValuePair(pair.first, pair.second)
+        }
+    }
+}
+
+@Composable
+fun KeyValuePair(key: String, value: String) {
+    // Text Composable with spannable string using different colors for key and value parameters
+    Text(
+        buildAnnotatedString {
+            withStyle(SpanStyle(color = Color.Red)) {
+                append(key)
+            }
+            append(" : ")
+            withStyle(SpanStyle(color = Color.Blue)) {
+                append(value)
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MaskedKeysDemoTheme {
+
+    }
+}
